17612b128b5d4e0d5634b57c54bf36e04a3b8406
listingslab <listingslab@gmail.com>
2019-12-10 15:41:50 +1000

    CRA

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  CssBaseline,
+  Button
+} from '@material-ui/core/';
+
+function App() {
+  return (
+    <div className="">
+      <CssBaseline />
+      <Button
+        variant={`contained`}
+        color={`primary`}
+      >
+        mapbox-redux-toolkit
+      </Button>
+    </div>
+  );
+}
+
+export default App;
